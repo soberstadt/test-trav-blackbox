b9a34a460b118c83d03661f70162ecfbb004e9b2
Spencer Oberstadt <soberstadt@gmail.com>
2015-11-25 08:33:06 -0500

    decrypt key

--- setup.rb
+++ setup.rb
@@ -4,15 +4,22 @@ puts 'clone secure stuff'
 puts `git clone https://github.com/soberstadt/blackbox-test.git`
 puts 'ls'
 puts `ls`
-Dir.chdir('blackbox') do
-  puts 'install blackbox'
-  puts `sudo make manual-install`
+#Dir.chdir('blackbox') do
+  #puts 'install blackbox'
+  #puts `sudo make manual-install`
+#end
+Dir.chdir('.') do
+  puts 'adding keys'
+  puts `whoami`
+  puts `sudo whoami`
+  puts `mkdir -m 0700 -p $HOME/.gnupg`
+  `cd $HOME/.gnupg && tar xpvf $TRAVIS_BUILD_DIR/keys.tar`
+  puts `ls $HOME/.gnupg`
 end
 Dir.chdir('blackbox-test') do
-  puts 'ls bin'
-  puts `ls /usr/local/bin`
   puts 'decrypt files'
-  puts `sudo /usr/local/bin/blackbox_postdeploy`
+  puts `gpg --import $HOME/.gnupg/pubring.gpg`
+  puts `../blackbox/bin/blackbox_postdeploy`
 end
 
 #puts 'puts setup'
