5d92c8ea8a9d04e3de32bfdc9ef86da350c3593b
Spencer Oberstadt <soberstadt@gmail.com>
2015-11-25 09:13:35 -0500

    cat secure file

--- setup.rb
+++ setup.rb
@@ -1,25 +1,13 @@
-puts 'clone blackbox'
-puts `git clone https://github.com/StackExchange/blackbox.git`
-puts 'clone secure stuff'
-puts `git clone https://github.com/soberstadt/blackbox-test.git`
-puts 'ls'
-puts `ls`
-#Dir.chdir('blackbox') do
-  #puts 'install blackbox'
-  #puts `sudo make manual-install`
-#end
-Dir.chdir('.') do
-  puts 'adding keys'
-  puts `whoami`
-  puts `sudo whoami`
-  puts `mkdir -m 0700 -p $HOME/.gnupg`
-  `cd $HOME/.gnupg && tar xpvf $TRAVIS_BUILD_DIR/keys.tar`
-  puts `ls $HOME/.gnupg`
-end
+p 'clone blackbox toolchain'
+p `git clone https://github.com/StackExchange/blackbox.git`
+p 'clone secure stuff'
+p `git clone https://github.com/soberstadt/blackbox-test.git`
+p 'adding keys'
+`mkdir -m 0700 -p $HOME/.gnupg`
+`tar xpvf keys.tar -C $HOME/.gnupg`
 Dir.chdir('blackbox-test') do
-  puts 'decrypt files'
+  puts 'decrypting files'
   puts `gpg --import $HOME/.gnupg/pubring.gpg`
   puts `../blackbox/bin/blackbox_postdeploy`
 end
 
-#puts 'puts setup'
